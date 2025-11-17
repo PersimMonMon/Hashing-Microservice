ec9d65fe04a0695ee9e5988be34e819a4c10c550
PersimMonMon <buitho@oregonstate.edu>
2025-11-16 21:18:01 -0800

    remove any imports not used

--- main.go
+++ main.go
@@ -1,10 +1,62 @@
+package main
 
 // add imports
 import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
-	"log"
+	"encoding/json"
 	"net/http"
 )
 
+// Create struct instances: map JSON and add struct tag to represent JSON fields
+type hashRequest struct {
+	Input string `json:"input"`
+	Hash  string `json:"hash"`
+}
+
+type hashResponse struct {
+	Hash string `json:"hash"`
+}
+
+// compute the hash
+func computeHash(input string, algorithm string) string {
+	switch algorithm {
+	case "sha1":
+		hash := sha1.Sum([]byte(input)) // convert to byte slice and later convert to hex string
+		return hex.EncodeToString(hash[:])
+	default:
+		hash := sha256.Sum256([]byte(input))
+		return hex.EncodeToString(hash[:])
+	}
+}
+
+// receive JSON POST request
+func hashHandler(w http.ResponseWriter, r *http.Request) {
+
+	// check if right request
+	if r.Method != http.MethodPost {
+		http.Error(w, "This microservice only takes in POST requests", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// create new struct instances
+	req := hashRequest{}
+
+	// read JSON request, convert to Go, return error if any
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil || req.Input == "" {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	// compute hash
+	result := computeHash(req.Input, req.Hash)
+
+	// set response to JSON and write back a response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(hashResponse{Hash: result})
+
+}
+
+// create server
